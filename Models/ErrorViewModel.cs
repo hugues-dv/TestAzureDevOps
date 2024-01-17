d6083d119478802dd02f309546e40c10c3d9a21a
hugues-dv <h2vregille@gmail.com>
2024-01-17 13:47:42 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TestAzureDevOps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
